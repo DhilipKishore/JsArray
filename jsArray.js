c09cf2eaff3299cd3f96845d26414050d4a5ceb3
DhilipKishore <99136490+DhilipKishore@users.noreply.github.com>
2022-03-31 21:44:57 +0530

    Add files via upload

---
+++ jsArray.js
@@ -0,0 +1,12 @@
+let color =["Red", "Green","Blue"]
+
+
+console.log(color.length);
+console.log(color);
+
+//Add new element in Array
+color[4]="Yellow";
+
+console.log(color.length);
+console.log(color[0]);
+console.log(color);
